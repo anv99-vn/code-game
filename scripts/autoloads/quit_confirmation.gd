e38f97e1a397a2ffe27b7c49f2f30deb079c1357
anv99-vn <anv99-vn@users.noreply.github.com>
2026-08-06 12:44:58 +0700

    Simplify quit confirmation: use project setting instead of duplicate handlers

--- scripts/autoloads/quit_confirmation.gd
+++ scripts/autoloads/quit_confirmation.gd
@@ -4,15 +4,6 @@ const QUIT_POPUP_SCENE := preload("res://scenes/quit_popup.tscn")
 var _popup: Control = null
 
 
-func _ready() -> void:
-	get_tree().get_root().go_back_requested.connect(show_quit_dialog)
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		show_quit_dialog()
-
-
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
 		show_quit_dialog()
